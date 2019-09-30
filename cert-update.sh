91aab0507d0991c616c3ebe6d7ad2e97f1d8e06f
Sam Maister <sam.maister@bbc.co.uk>
2019-09-30 14:55:46 +0100

    add optional aliasing, expose cloud-ca.pem in etc/pki

--- cert-update.sh
+++ cert-update.sh
@@ -24,11 +24,16 @@
 KEYSTORE_DIR='/etc/pki'
 TMP_DIR='/tmp'
 CA_BUNDLE_DIR="$KEYSTORE_DIR/tls/certs"
+MORPH_NPM_COMMAND="npm --registry https://npm.morph.int.tools.bbc.co.uk --cert=\"$(cat $KEYSTORE_DIR/certificate.pem)\" --key=\"$(cat $KEYSTORE_DIR/certificate.pem)\" --cafile=$CA_BUNDLE_DIR/ca-bundle.crt"
+MORPH_NPM_COMMAND_STRING='npm --registry https://npm.morph.int.tools.bbc.co.uk --cert=\"$(cat /etc/pki/certificate.pem)\" --key=\"$(cat /etc/pki/certificate.pem)\" --cafile=/etc/pki/tls/certs/ca-bundle.crt'
 BACKUP_ROOT_DIR="$HOME/Certs/backups"
 BACKUP_DIR="$BACKUP_ROOT_DIR/$(date +"%d%m%Y-%H%M%S")"
 SSH_CONFIG_DIR="$HOME/.ssh"
 WORKSPACE_DIR="$HOME/workspace"
 REQUIRED_DIRS=($KEYSTORE_DIR $TMP_DIR $BACKUP_ROOT_DIR $SSH_CONFIG_DIR $WORKSPACE_DIR)
+green=`tput setaf 2`
+magenta=`tput setaf 5`
+reset=`tput sgr0`
 
 function politeSudo() { 
     sudo -p "Sudo required for this step, please enter your password: " "$@"
@@ -66,14 +71,14 @@ backup $SSH_CONFIG_DIR 'id_rsa.pub'
 backup $WORKSPACE_DIR 'dev.bbc.co.uk.p12'
 backup $CA_BUNDLE_DIR 'ca-bundle.crt'
  
-read -p $'\nPlease enter your certificate password: ' -s CERT_PASSWORD
+read -sp $'\nPlease enter your certificate password: ' CERT_PASSWORD
  
 echo "\nConverting ($1) to PEM..."
 politeSudo cp $1 "$KEYSTORE_DIR/certificate.p12"
 politeSudo openssl pkcs12 -in $1 -out "$WORKSPACE_DIR/certificate.pem" -nodes -clcerts -passin "pass:$CERT_PASSWORD"
 politeSudo cp "$WORKSPACE_DIR/certificate.pem" "$KEYSTORE_DIR/certificate.pem"
  
-read -p $'\nPlease enter your private key password (or enter to use your certificate password): ' -s PRIVATE_KEY_PASSWORD
+read -sp $'\nPlease enter your private key password (or enter to use your certificate password): ' PRIVATE_KEY_PASSWORD
 PRIVATE_KEY_PASSWORD=${PRIVATE_KEY_PASSWORD:-$CERT_PASSWORD}
  
 echo "\nCreating SSH config..."
@@ -95,10 +100,17 @@ cat "$TMP_DIR/root-cas.pem" "$TMP_DIR/cloud-ca.pem" > "$TMP_DIR/ca-bundle.crt"
 politeSudo mv "$TMP_DIR/ca-bundle.crt" "$CA_BUNDLE_DIR/ca-bundle.crt"
 politeSudo chmod og+r "$CA_BUNDLE_DIR/ca-bundle.crt"
  
-echo 'Updating NPM alias...'
-alias npm="npm --registry https://npm.morph.int.tools.bbc.co.uk --cert=\"$(cat $KEYSTORE_DIR/certificate.pem)\" --key=\"$(cat $KEYSTORE_DIR/certificate.pem)\" --cafile=$CA_BUNDLE_DIR/ca-bundle.crt"
- 
 echo 'Removing temporary files...'
 rm "$TMP_DIR/root-cas.pem" "$TMP_DIR/cloud-ca.pem"
 
 echo "\nCertificates updated."
+
+read -p $'\nPlease enter an alias for morph npm (or press enter if you do not wish to set an alias): ' MORPH_NPM_ALIAS
+
+if [ $MORPH_NPM_ALIAS ]; then
+    alias $MORPH_NPM_ALIAS="$MORPH_NPM_COMMAND"
+    echo "Alias set for '$MORPH_NPM_ALIAS'.  You should add the following command to your shell config in order to persist the alias:"
+    echo "\n${magenta}alias $MORPH_NPM_ALIAS=\"$MORPH_NPM_COMMAND_STRING\""
+fi
+
+echo "\n${green}Complete.${reset}"
